a0c1c7f704caea010bd558db707045bcfd4347c4
Bohdan3457 <bohdankhomenko04@gmail.com>
2026-08-21 19:20:48 +0200

    Add migration for movie image

---
+++ cinema/migrations/0002_movie_image.py
@@ -0,0 +1,17 @@
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cinema', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='movie',
+            name='image',
+            field=models.ImageField(null=True, upload_to='uploads/'),
+        ),
+    ]
